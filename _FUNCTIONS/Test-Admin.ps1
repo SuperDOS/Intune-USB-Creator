91c73f3eb4711c1954e3ac9fc858ad9a6052198f
SuperDOS <superdos@gmail.com>
2026-02-16 22:25:21 +0100

    clean line breaks

--- _FUNCTIONS/Test-Admin.ps1
+++ _FUNCTIONS/Test-Admin.ps1
@@ -2,4 +2,3 @@ function Test-Admin {
     $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
     $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
-
