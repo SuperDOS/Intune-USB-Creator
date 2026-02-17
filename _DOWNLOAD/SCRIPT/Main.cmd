772c7460d7d138579698f848e44dc8d9187dea27
SuperDOS <superdos@gmail.com>
2026-02-17 09:29:26 +0100

    Enhance Main.cmd with driver loading and WiFi support
    Added loading of WinPE drivers and WiFi support comments.

--- _DOWNLOAD/SCRIPT/Main.cmd
+++ _DOWNLOAD/SCRIPT/Main.cmd
@@ -1,2 +1,15 @@
 title INTUNE USB DEPLOYMENT
+
+::Load WinPE Drivers
+for /r "%~dp0WinPE" %%F in (*.inf) do (
+    echo Loading WinPE driver: %%F
+    drvload "%%F"
+)
+
+::WiFi Support using WinRE
+REM net start wlansvc
+REM netsh wlan add profile filename= %~dp0wificonf.xml
+REM netsh wlan connect name=@WIFISSID ssid=@WIFISSID
+REM ping localhost -n 30 >nul
+
 call %~dp0pwsh\pwsh.exe %~dp0invoke-provision.ps1
